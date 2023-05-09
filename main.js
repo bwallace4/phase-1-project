3663ff94760b546f83fa77253dd911b4cdbf756c
Brandon Wallace <brandonwallace403@gmail.com>
2023-05-09 16:56:04 -0400

    changes to main.js

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const shoelist= document.querySelector(".shoe-list")
 console.log(shoelist);
-fetch("http://localhost:3000/shoes").then(response => response.json())
-.then((shoes) => {
-console.log("do i get tasks",shoes)
-shoes.forEach(shoebox)
+fetch(" http://localhost:3000/shoe").then(response => response.json())
+.then((shoe) => {
+shoe.forEach(shoebox)
 });
+
 const shoebox = (shoedata) => {
+   console.log( shoedata.name)
+console.log(shoedata)
 const shoeDiv = document.createElement("div")
 const shoePara = document.createElement("p")
-const editBtn = document.createElement("button")
-const deleteBtn = document.createElement("button")
-const shoeInfo = document.createElement("p")
-shoePara.textContent = shoedata.shoes
+// const editBtn = document.createElement("button")
+// const deleteBtn = document.createElement("button")
+// const shoeInfo = document.createElement("p")
+shoePara.textContent = shoedata.name
 shoeDiv.append(shoePara)
 shoelist.append(shoeDiv)
 }
