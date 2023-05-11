0e1cae6d90d134045582cd9e9632fd6ea255472c
Brandon Wallace <brandonwallace403@gmail.com>
2023-05-11 14:30:05 -0400

    adding evenlistner to main.js

--- main.js
+++ main.js
@@ -7,19 +7,40 @@ shoe.forEach(shoebox)
 });
 
 const shoebox = (shoedata) => {
-console.log(shoedata)
+
 const shoeDiv = document.createElement("div")
 const shoePara = document.createElement("p")
 const shoescript = document.createElement("div")
 const shoeInfo = document.createElement("div")
-// const editBtn = document.createElement("button")
-// const deleteBtn = document.createElement("button")
+const image = document.createElement("img")
+const alt_image = document.createElement("img")
+
+image.src = shoedata.image
+image.className = "shoeImage"
+alt_image.src = shoedata.alt_image
 shoePara.textContent = shoedata.name
 shoeDiv.textContent = shoedata.id
 shoeInfo.textContent = shoedata.type
 shoescript.textContent = shoedata.description
+
 shoeInfo.append(shoescript)
+shoescript.append(image)
 shoeDiv.append(shoePara)
 shoelist.append(shoeDiv)
 shoePara.append(shoeInfo)
+
+let toggle = true;
+image.addEventListener('click', function(){
+   toggle = !toggle;
+   if("toggle"){
+    image.src = shoedata.alt_image
+   }else{
+    image.src = "shoedata.alt-image"
+   
+   }
+    
+   
+  
+
+})
 }
